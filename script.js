ecffa1bf28dff6c09a58dd396ea2f06080f2c8d7
Andrew Malandrin <36206556+andrewmalandrin@users.noreply.github.com>
2022-04-01 19:38:45 -0300

    Corrigidos apontamentos pra imagens
    Corrigidos apontamentos pra imagens

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ function exibirProjetos(){
   let eventos = []
   let htmlCode = ""
 
-  projetos.push({"projeto":"Conversor de Temperatura", "evento":"Imersão Dev", "url":"https://codepen.io/andrew_malandrin/pen/ZEaddVp", "img":"/img/conversortemperatura.jpg"})
-  projetos.push({"projeto":"Super Trunfo - Folclore Nacional Raro", "evento":"Imersão Dev", "url":"https://codepen.io/andrew_malandrin/pen/JjMXXjd", "img":"/img/supertrunfo.jpg"})
-  projetos.push({"projeto":"Pelada Pro ⚽", "evento":"Imersão Dev", "url":"https://codepen.io/andrew_malandrin/pen/BaJopay", "img":"/img/peladapro.jpg"})
-  projetos.push({"projeto":"Aluraflix", "evento":"Imersão Dev", "url":"https://codepen.io/andrew_malandrin/pen/GRyJmMe", "img":"/img/aluraflix.jpg"})
-  projetos.push({"projeto":"Soccer Badges", "evento":"Imersão Dev", "url":"https://codepen.io/andrew_malandrin/pen/JjMoMJb", "img":"/img/soccerbadges.jpg"})
-  projetos.push({"projeto":"Calculadora de Média Simples", "evento":"Imersão Dev", "url":"https://codepen.io/andrew_malandrin/pen/podmYjo", "img":"/img/calculadoramedia.jpg"})
-  projetos.push({"projeto":"Adivinhe o Número", "evento":"Imersão Dev", "url":"https://codepen.io/andrew_malandrin/pen/RwjXezp", "img":"/img/mentalista.jpg"})
-  projetos.push({"projeto":"Guia Dev", "evento":"7 Days of Code", "url":"https://codepen.io/andrew_malandrin/pen/QWadGzq", "img":"/img/guiadev.jpg"})
-  projetos.push({"projeto":"Advinhe o Número 2.0", "evento":"7 Days of Code", "url":"", "img":"/img/mentalista2.jpg"})
-  projetos.push({"projeto":"Lista de Compras", "evento":"7 Days of Code", "url":"https://codepen.io/andrew_malandrin/pen/ZEvKByL", "img":"/img/listacompras.jpg"})
-  projetos.push({"projeto":"Calculadora simples", "evento":"7 Days of Code", "url":"https://codepen.io/andrew_malandrin/pen/VwybGYj", "img":"/img/calculadora.jpg"})
-  projetos.push({"projeto":"Modelo de Portfolio", "evento":"Curso - Bootstrap", "url":"https://github.com/andrewmalandrin/aula_bootstrap", "img":"/img/modeloportfolio.jpg"})
+  projetos.push({"projeto":"Conversor de Temperatura", "evento":"Imersão Dev", "url":"https://codepen.io/andrew_malandrin/pen/ZEaddVp", "img":"./img/conversortemperatura.jpg"})
+  projetos.push({"projeto":"Super Trunfo - Folclore Nacional Raro", "evento":"Imersão Dev", "url":"https://codepen.io/andrew_malandrin/pen/JjMXXjd", "img":"./img/supertrunfo.jpg"})
+  projetos.push({"projeto":"Pelada Pro ⚽", "evento":"Imersão Dev", "url":"https://codepen.io/andrew_malandrin/pen/BaJopay", "img":"./img/peladapro.jpg"})
+  projetos.push({"projeto":"Aluraflix", "evento":"Imersão Dev", "url":"https://codepen.io/andrew_malandrin/pen/GRyJmMe", "img":"./img/aluraflix.jpg"})
+  projetos.push({"projeto":"Soccer Badges", "evento":"Imersão Dev", "url":"https://codepen.io/andrew_malandrin/pen/JjMoMJb", "img":"./img/soccerbadges.jpg"})
+  projetos.push({"projeto":"Calculadora de Média Simples", "evento":"Imersão Dev", "url":"https://codepen.io/andrew_malandrin/pen/podmYjo", "img":"./img/calculadoramedia.jpg"})
+  projetos.push({"projeto":"Adivinhe o Número", "evento":"Imersão Dev", "url":"https://codepen.io/andrew_malandrin/pen/RwjXezp", "img":"./img/mentalista.jpg"})
+  projetos.push({"projeto":"Guia Dev", "evento":"7 Days of Code", "url":"https://codepen.io/andrew_malandrin/pen/QWadGzq", "img":"./img/guiadev.jpg"})
+  projetos.push({"projeto":"Advinhe o Número 2.0", "evento":"7 Days of Code", "url":"", "img":"./img/mentalista2.jpg"})
+  projetos.push({"projeto":"Lista de Compras", "evento":"7 Days of Code", "url":"https://codepen.io/andrew_malandrin/pen/ZEvKByL", "img":"./img/listacompras.jpg"})
+  projetos.push({"projeto":"Calculadora simples", "evento":"7 Days of Code", "url":"https://codepen.io/andrew_malandrin/pen/VwybGYj", "img":"./img/calculadora.jpg"})
+  projetos.push({"projeto":"Modelo de Portfolio", "evento":"Curso - Bootstrap", "url":"https://github.com/andrewmalandrin/aula_bootstrap", "img":"./img/modeloportfolio.jpg"})
 
   //projetos.push({"projeto":"", "evento":"", "url":"", "img":""})
 
